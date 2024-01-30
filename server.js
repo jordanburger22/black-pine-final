33e3786c31f6ac620e9fd198bb2ba09b5f61d794
Jordan Burger <jordanburger22@gmail.com>
2024-01-30 11:00:28 -0700

    added server code

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const {expressjwt} = require('express-jwt')
+const path = require('path')
 
 
 app.use(express.json())
 app.use(morgan('dev'))
-
+app.use(express.static(path.join(__dirname, "client", "dist")));
 mongoose.set('strictQuery', true)
 mongoose.connect(process.env.MONGO_URI, () => console.log('connected to database'))
 
@@ -28,7 +29,9 @@ app.use((err, req, res, next) => {
     return res.send({errMsg: err.message})
 })
 
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html")); // middleware for deployment // dist for vite
+  });
 
 app.listen(5550, () => {
     console.log('server is running on port 5550')
